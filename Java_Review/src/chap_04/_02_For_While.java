7735299c23243fdec70db9067430284fc59602a0
sungwoo <seouk77@gmail.com>
2023-01-21 22:52:12 +0900

    Add array, function practice

--- Java_Review/src/chap_04/_02_For_While.java
+++ Java_Review/src/chap_04/_02_For_While.java
@@ -1,12 +1,14 @@
 package chap_04;
 
-public class _02_For {
+public class _02_For_While {
     public static void main(String[] args) {
+        // For 반복문
         for (int i = 0; i < 10; i++) {
             System.out.print(i + " ");
         }
         System.out.println();
 
+        // While 반복문
         int distance = 25;
         int move = 0;
         while (move < 25) {
@@ -14,6 +16,17 @@ public class _02_For {
             move += 3;
         }
         System.out.println("도착!");
-        //
+
+
+        // 오른쪽 별 찍기
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 4-i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j < i+1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
     }
 }

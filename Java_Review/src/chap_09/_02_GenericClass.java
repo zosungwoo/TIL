9c8010417aa532d8791cf2fff7853edd81103b25
sungwoo <seouk77@gmail.com>
2023-02-01 23:57:41 +0900

    Add java generics practice 2

---
+++ Java_Review/src/chap_09/_02_GenericClass.java
@@ -0,0 +1,8 @@
+package chap_09;
+
+public class _02_GenericClass {
+    public static void main(String[] args) {
+        // 제네릭 클래스
+
+    }
+}
---
+++ Java_Review/src/chap_09/coffee/CoffeeByNumber.java
@@ -0,0 +1,11 @@
+package chap_09.coffee;
+
+public class CoffeeByNumber {
+    public int waitingNumber;
+
+    public CoffeeByNumber(int waitingNumber) {
+        this.waitingNumber = waitingNumber;
+    }
+
+
+}

2491cb34a8b499809639e801a91bd9773187ea5e
sungwoo <seouk77@gmail.com>
2023-02-27 00:34:07 +0900

    chore: replit -> pycharm

--- Java_Review/src/chap_08/detector/Detectable.java
+++
@@ -1,6 +0,0 @@
-package chap_08.detector;
-
-public interface Detectable {
-    String name = "감지자";  // 기본적으로 public static final String name으로 선언됨
-    void detect();  // 기본적으로 public abstract void detect()로 선언됨
-}
--- Java_Review/src/chap_08/detector/FireDetector.java
+++
@@ -1,8 +0,0 @@
-package chap_08.detector;
-
-public class FireDetector implements Detectable{
-    @Override
-    public void detect() {
-        System.out.println("화재를 감지합니다.");
-    }
-}

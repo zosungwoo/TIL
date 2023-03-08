2d69c84e2cd079a8eef4f473aef000cef42653e1
Sungwoo Cho <seouk77@gmail.com>
2023-03-08 23:59:23 +0900

    Add spring boot api test2

--- Spring/1. Spring Boot with book/2. api/src/main/java/com/springboot/api/controller/GetController.java
+++ Spring/1. Spring Boot with book/2. api/src/main/java/com/springboot/api/controller/GetController.java
@@ -1,9 +1,8 @@
 package com.springboot.api.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/v1/get-api")
@@ -14,8 +13,40 @@ public class GetController {
         return "Hello World";
     }
 
+    // http://localhost:8080/api/v1/get-api/name
     @GetMapping(value = "/name")
     public String getName(){
         return "Flature";
     }
+
+    // http://localhost:8080/api/v1/get-api/variable1/{String 값}
+    @GetMapping(value = "/variable1/{variable}")
+    public String getVariable1(@PathVariable String variable){
+        return variable;
+    }
+
+    // http://localhost:8080/api/v1/get-api/variable2/{String 값}
+    @GetMapping(value = "/variable2/{variable}")
+    public String getVariable2(@PathVariable("variable") String var){
+        return var;
+    }
+
+    // http://localhost:8080/api/v1/get-api/request1?name=value1&email=value2&organization=value3
+    @GetMapping(value = "/request1")
+    public String getRequestParam1(@RequestParam String name, @RequestParam String email, @RequestParam String organization){
+        return name + " " + email + " " + organization;
+    }
+
+    // http://localhost:8080/api/v1/get-api/request2?key1=value1&key2=value2
+    @GetMapping(value = "/request2")
+    public String getRequestParam2(@RequestParam Map<String, String> param){  // 어떤 값이 올지 모를 때 Map 활용
+        StringBuilder sb = new StringBuilder();
+
+        param.entrySet().forEach(map -> {
+            sb.append(map.getKey() + " : " + map.getValue() + "\n");
+        });
+
+        return sb.toString();
+    }
+
 }

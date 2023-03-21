727e731f5065aa98f0fd2e2c1132156e2a13fb9e
Sungwoo Cho <seouk77@gmail.com>
2023-03-21 09:00:38 +0900

    Add spring boot 'logback'

--- Spring/1. Spring Boot with book/2. api/src/main/java/com/springboot/api/controller/GetController.java
+++ Spring/1. Spring Boot with book/2. api/src/main/java/com/springboot/api/controller/GetController.java
@@ -3,6 +3,8 @@ package com.springboot.api.controller;
 import com.springboot.api.dto.MemberDto;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
@@ -10,21 +12,28 @@ import java.util.Map;
 @RestController
 @RequestMapping("/api/v1/get-api")
 public class GetController {
+
+    private final Logger LOGGER = LoggerFactory.getLogger(GetController.class);
+
+
     // http://localhost:8080/api/v1/get-api/hello
     @RequestMapping(value = "/hello", method = RequestMethod.GET)
     public String getHello() {
+        LOGGER.info("getHello 메서드가 호출되었습니다.");
         return "Hello World";
     }
 
     // http://localhost:8080/api/v1/get-api/name
     @GetMapping(value = "/name")
     public String getName(){
+        LOGGER.info("getName 메서드가 호출되었습니다.");
         return "Flature";
     }
 
     // http://localhost:8080/api/v1/get-api/variable1/{String 값}
     @GetMapping(value = "/variable1/{variable}")
     public String getVariable1(@PathVariable String variable){
+        LOGGER.info("@PathVariable을 통해 들어온 값: {}", variable);
         return variable;
     }
 

165eb778a7f8731d6ca2765fe368c8e81b357607
Sungwoo Cho <seouk77@gmail.com>
2023-03-26 11:54:22 +0900

    add algorithm problem solutions

---
+++ Algorithm/[Book] Python For Coding Test/Part2. Practice/3. DFS_BFS/0-2. BFS.py
@@ -0,0 +1,32 @@
+from collections import deque
+
+def bfs(graph, start, visited):
+    queue = deque([start])
+
+    visited[start] = True
+
+    while queue:
+        v = queue.popleft()
+        print(v, end = ' ')
+
+        for i in graph[v]:
+            if not visited[i]:
+                queue.append(i)
+                visited[i] = True
+
+
+graph = [
+    [],
+    [2, 3, 8],  # 1번 노드 ~
+    [1, 7],
+    [1, 4, 5],
+    [3, 5],
+    [3, 4],
+    [7],
+    [2, 6, 8],
+    [1 ,7]  # ~ 8번 노드
+]
+
+visited = [False] * 9  # [False for _ in range(9)] -> 더 느림
+
+bfs(graph, 1, visited)

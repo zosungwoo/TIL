9afbaa6da0c5e935772a58f855963c194346b566
Sungwoo Cho <seouk77@gmail.com>
2023-09-14 15:49:05 +0900

    add database integration

--- Database Programming/DBP/src/main/java/org/example/App.java
+++ Database Programming/DBP/src/main/java/org/example/App.java
@@ -1,13 +1,23 @@
 package org.example;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 /**
  * Hello world!
  *
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+    public static void main(String[] args) throws SQLException {
+        Connection connection = DriverManager.getConnection("jdbc:mariadb://localhost:3306", "root", "1234");
+
+        Statement stat = connection.createStatement();
+        stat.executeUpdate("create database dbp2;");
+        stat.executeUpdate("use dbp2;");
+
+        connection.close();
     }
 }

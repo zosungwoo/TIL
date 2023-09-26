4ed8ed8bdd4e3ea50e31d01099a5ebc3639f8423
Sungwoo Cho <seouk77@gmail.com>
2023-09-27 00:54:51 +0900

    [DB Programming] DDL java programming

---
+++ Database Programming/DBP/src/main/java/org/example/week4_1/Test1.java
@@ -0,0 +1,46 @@
+package org.example.week4_1;
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
+public class Test1 {
+    public static void main(String[] args) throws Exception{
+        String id = "root";
+        String password = "1234";
+
+        Connection connection = DriverManager.getConnection("jdbc:mariadb://localhost:3306", id, password);
+
+        Statement stat = connection.createStatement();
+//        stat.executeUpdate("CREATE DATABASE IF NOT EXISTS dbp");
+        stat.executeUpdate("CREATE OR REPLACE DATABASE dbp");
+        stat.executeUpdate("USE dbp;");
+        stat.executeUpdate(
+                "CREATE TABLE IF NOT EXISTS customer2 (" +
+                        "customer VARCHAR(50), customer_street VARCHAR(50), customer_city VARCHAR(50), latitude DOUBLE, longitude DOUBLE, last_update DATE" +
+                        ");"
+        );
+
+        // latitude longitude 수정
+        stat.executeUpdate("ALTER TABLE customer2 DROP COLUMN latitude");
+        stat.executeUpdate("ALTER TABLE customer2 DROP COLUMN longitude");
+
+        stat.executeUpdate("ALTER TABLE customer2 RENAME COLUMN customer TO customer_name");
+        stat.executeUpdate("ALTER TABLE customer2 ADD COLUMN (geopoint POINT)");
+        stat.executeUpdate("ALTER TABLE customer2 MODIFY customer_street VARCHAR(100)");
+
+
+
+
+//        stat.executeUpdate("DROP TABLE IF EXISTS customer2");
+//        System.out.println(stat.getWarnings());
+
+
+
+
+        connection.close();
+
+
+    }
+}
---
+++ Database Programming/DBP/src/main/java/org/example/week4_1/Test2.java
@@ -0,0 +1,45 @@
+package org.example.week4_1;
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
+public class Test2 {
+    public static void main(String[] args) throws Exception{
+        String id = "root";
+        String password = "1234";
+
+        Connection connection = DriverManager.getConnection("jdbc:mariadb://localhost:3306", id, password);
+
+        Statement stat = connection.createStatement();
+        stat.executeUpdate("CREATE OR REPLACE DATABASE dbp");
+        stat.executeUpdate("USE dbp;");
+        stat.executeUpdate(
+                "CREATE TABLE IF NOT EXISTS account (" +
+                        "account_number varchar(50) unique, branch_name varchar(50), balance int" +
+                        ");"
+        );
+
+//        stat.executeUpdate("insert into account values('A-101', 'Downtown', 500)");
+//        stat.executeUpdate("insert into account values(null, 'Mianus', 900)");  // not null 오류
+
+        stat.executeUpdate("INSERT INTO account VALUES ('A-101', 'Downtown', 500)");
+        stat.executeUpdate("INSERT INTO account VALUES ('A-102', 'Perryridge', 400)");
+        stat.executeUpdate("INSERT INTO account VALUES (null, 'Brighton', 900)");
+        stat.executeUpdate("INSERT INTO account VALUES (null, 'Mianus', 700)");  // null은 unique 대상에서 제외! (당연히 그래야 실용적임)
+//        stat.executeUpdate("INSERT INTO account VALUES ('A-102', 'Perryridge', 300)");
+
+
+
+//        stat.executeUpdate("DROP TABLE IF EXISTS customer2");
+//        System.out.println(stat.getWarnings());
+
+
+
+
+        connection.close();
+
+
+    }
+}

9450259ba18caee64b8e58a315525d1842d8d596
Sungwoo Cho <seouk77@gmail.com>
2023-10-12 00:41:53 +0900

    [Data Engineering] Java LinkedList Data Handling 2

--- Data Enginnering/DE1/src/main/java/org/example/linkedlist/LinkedListExample14.java
+++ Data Enginnering/DE1/src/main/java/org/example/linkedlist/LinkedListExample14.java
@@ -9,9 +9,9 @@ import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 
-public class LinkedListExample13 {
-    public static List<Email> getArrayList() throws IOException {  // App13 수정
-        List<Email> data = new ArrayList<>();
+public class LinkedListExample14 {
+    public static ArrayList<Email> getArrayList() throws IOException {  // App13 수정
+        ArrayList<Email> data = new ArrayList<>();
 
         BufferedReader br = new BufferedReader(new FileReader("/Users/sungwoo/Desktop/TIL/Data Enginnering/DE1/src/main/java/org/example/data/email.txt"));
         int index = 0;
@@ -30,8 +30,8 @@ public class LinkedListExample13 {
         br.close();
         return data;
     }
-    public static List<Email> getLinkedList() throws IOException {  // App13 수정
-        List<Email> data = new LinkedList<>();
+    public static LinkedList<Email> getLinkedList() throws IOException {  // App13 수정
+        LinkedList<Email> data = new LinkedList<>();
 
         BufferedReader br = new BufferedReader(new FileReader("/Users/sungwoo/Desktop/TIL/Data Enginnering/DE1/src/main/java/org/example/data/email.txt"));
         int index = 0;
@@ -51,39 +51,40 @@ public class LinkedListExample13 {
         return data;
     }
     public static void main(String[] args) throws IOException {
-        List<Email> array = getArrayList();
-        List<Email> link = getLinkedList();
+        ArrayList<Email> array = getArrayList();
+        LinkedList<Email> link = getLinkedList();  // 이번엔 LinkedList로 받아주게 함수를 수정 (그래야 poll 사용 가능) ArrayList도 마찬가지
 
         long p1 = System.currentTimeMillis();
-        int from = 0;
-        for (Email email :
-                array) {
-            from += email.getFrom();
+        while (true) {
+            if (array.size() > 0) {
+                array.remove(0);
+            } else {
+                break;
+            }
         }
-        System.out.println(System.currentTimeMillis()-p1);
+        System.out.println(System.currentTimeMillis() - p1);  // 처음에 느렸다가 점점 빨라짐 (뒤의 데이터를 앞으로 shift 해줘야함)
 
         long p2 = System.currentTimeMillis();
-        from = 0;
-        for (Email email:
-             link) {
-            from += email.getFrom();
+        while (true) {
+            Email email = link.poll();
+            if(email == null)
+                break;
         }
         System.out.println(System.currentTimeMillis() - p2);
 
-        long p3 = System.currentTimeMillis();
-        from = 0;
-        for (int i = 0; i < array.size(); i++) {
-            from += array.get(i).getFrom();
-        }
-        System.out.println(System.currentTimeMillis()-p3);
 
-        long p4 = System.currentTimeMillis();
-        from = 0;
-        for (int i = 0; i < link.size(); i++) {
-            from += link.get(i).getFrom();  // 맨 앞에서 맨 뒤까지 순회해야하므로 오래 걸림 (따라서 절대 인덱스로 순회하면 안됨!!!!!)
+        ArrayList<Email> array2 = getArrayList();
+        long p3 = System.currentTimeMillis();
+        while (true) {
+            if (array2.size() > 0) {
+                array2.remove(array2.size()-1);
+            } else {
+                break;
+            }
         }
-        System.out.println(System.currentTimeMillis()-p4);
-        // get -> node 구현을 보면 앞 절반까지는 앞에서부터 찾고, 뒤 절반까지는 뒤에서부터 찾아서, 점점 느려지다가 210000쯤부터는 420000까지 다시 빨라짐
+        System.out.println(System.currentTimeMillis() - p3);
+        // 스택으로서의 구현으로 shift가 일어나지 않음. but 메모리 효율 측면에서는 비효율적임. 기존 공간이 아예 사라지는 것이 아닌 null로 채워지기 때문
+        // (Email 객체에서 null 담는 공간으로 변했기 때문에 GC가 약간 공간을 확보해주긴 함)
 
     }
 }

47ecd09728b98f7b880a570c88dfb5a4b300f017
Sungwoo Cho <seouk77@gmail.com>
2023-11-01 09:25:41 +0900

    [Data Engineering] Java MyLinkedList Implementation

---
+++ Data Enginnering/DE1/src/main/java/org/example/linkedlist/implimentation/MyLinkedList.java
@@ -0,0 +1,192 @@
+package org.example.linkedlist.implimentation;
+
+import java.util.*;
+
+public class MyLinkedList<E> implements List<E>, Queue<E> {
+
+    private MyNode<E> first;
+    private MyNode<E> last;
+    private int size;
+
+    public MyLinkedList() {
+        first = null;
+        last = null;
+        size = 0;
+    }
+
+    public MyLinkedList(Collection<? extends E> c) {
+        this();
+        for (E value :
+                c) {
+            add(value);
+        }
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    @Override
+    public boolean contains(Object o) {
+        return false;
+    }
+
+    @Override
+    public Iterator<E> iterator() {
+        return null;
+    }
+
+    @Override
+    public Object[] toArray() {
+        return new Object[0];
+    }
+
+    @Override
+    public <T> T[] toArray(T[] a) {
+        return null;
+    }
+
+    @Override
+    public boolean add(E e) {
+        if (first == null) {
+            MyNode<E> newNode = new MyNode<>(null, e, null);
+            first = newNode;
+            last = newNode;
+            size++;
+        } else {
+            MyNode<E> newNode = new MyNode<>(last, e, null);
+            last.setNext(newNode);
+            last = newNode;
+            size++;
+        }
+        return true;
+    }
+
+    @Override
+    public boolean offer(E e) {
+        return false;
+    }
+
+    @Override
+    public E remove() {
+        return null;
+    }
+
+    @Override
+    public E poll() {
+        return null;
+    }
+
+    @Override
+    public E element() {
+        return null;
+    }
+
+    @Override
+    public E peek() {
+        return null;
+    }
+
+    @Override
+    public boolean remove(Object o) {
+        return false;
+    }
+
+    @Override
+    public boolean containsAll(Collection<?> c) {
+        return false;
+    }
+
+    @Override
+    public boolean addAll(Collection<? extends E> c) {
+        return false;
+    }
+
+    @Override
+    public boolean addAll(int index, Collection<? extends E> c) {
+        return false;
+    }
+
+    @Override
+    public boolean removeAll(Collection<?> c) {
+        return false;
+    }
+
+    @Override
+    public boolean retainAll(Collection<?> c) {
+        return false;
+    }
+
+    @Override
+    public void clear() {
+
+    }
+
+    @Override
+    public E get(int index) {
+        return null;
+    }
+
+    @Override
+    public E set(int index, E element) {
+        return null;
+    }
+
+    @Override
+    public void add(int index, E element) {
+
+    }
+
+    @Override
+    public E remove(int index) {
+        return null;
+    }
+
+    @Override
+    public int indexOf(Object o) {
+        return 0;
+    }
+
+    @Override
+    public int lastIndexOf(Object o) {
+        return 0;
+    }
+
+    @Override
+    public ListIterator<E> listIterator() {
+        return null;
+    }
+
+    @Override
+    public ListIterator<E> listIterator(int index) {
+        return null;
+    }
+
+    @Override
+    public List<E> subList(int fromIndex, int toIndex) {
+        return null;
+    }
+
+    @Override
+    public String toString() {
+        if (size == 0) {
+            return "[]";
+        }
+        String result = "[";
+        result += first.getItem();
+        MyNode<E> cursor = first.getNext();
+        while(cursor != null)
+        {
+            result += "," + cursor.getItem();
+            cursor = cursor.getNext();
+        }
+        result += "]";
+        return result;
+    }
+}
---
+++ Data Enginnering/DE1/src/main/java/org/example/linkedlist/implimentation/MyLinkedListExample1.java
@@ -0,0 +1,12 @@
+package org.example.linkedlist.implimentation;
+
+public class MyLinkedListExample1 {
+    public static void main(String[] args) {
+        MyLinkedList<Integer> list = new MyLinkedList<>();
+        list.add(3);
+        list.add(5);
+        list.add(7);
+
+        System.out.println(list);
+    }
+}

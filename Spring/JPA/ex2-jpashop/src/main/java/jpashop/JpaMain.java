2f591cb34f50a0eb60958e3ea34fce5069b796c6
Sungwoo Cho <seouk77@gmail.com>
2024-03-18 18:37:57 +0900

    [JPA] 엔티티 매핑

---
+++ Spring/JPA/ex2-jpashop/src/main/java/jpashop/JpaMain.java
@@ -0,0 +1,26 @@
+package jpashop;
+
+import jakarta.persistence.*;
+
+public class JpaMain {
+
+    public static void main(String[] args) {
+
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+
+
+            tx.commit();  // 커밋 직전에 업데이트 쿼리를 만들어 날림
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+            emf.close();
+        }
+    }
+}

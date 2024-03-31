e37526849df8f64f5aa4ae27bfcfd0b997f0d691
Sungwoo Cho <seouk77@gmail.com>
2024-03-31 16:05:17 +0900

    [JPA] 지연 로딩, 영속성 전이 설정

--- Spring/JPA/ex2-jpashop/src/main/java/jpashop/domain/Delivery.java
+++ Spring/JPA/ex2-jpashop/src/main/java/jpashop/domain/Delivery.java
@@ -1,9 +1,6 @@
 package jpashop.domain;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import jakarta.persistence.OneToOne;
+import jakarta.persistence.*;
 
 @Entity
 public class Delivery extends BaseEntity {
@@ -18,6 +15,6 @@ public class Delivery extends BaseEntity {
 
     private DeliveryStatus status;
 
-    @OneToOne(mappedBy = "delivery")
+    @OneToOne(mappedBy = "delivery", fetch = FetchType.LAZY)
     private Order order;
 }
--- Spring/JPA/ex2-jpashop/src/main/java/jpashop/domain/Order.java
+++ Spring/JPA/ex2-jpashop/src/main/java/jpashop/domain/Order.java
@@ -13,11 +13,11 @@ public class Order extends BaseEntity {
     @Column(name = "ORDER_ID")
     private Long id;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "MEMBER_ID")
     private Member member;
 
-    @OneToOne
+    @OneToOne(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
     @JoinColumn(name = "DELIVERY_ID")
     private Delivery delivery;
 
@@ -26,7 +26,7 @@ public class Order extends BaseEntity {
     @Enumerated(EnumType.STRING)
     private OrderStatus status;
 
-    @OneToMany(mappedBy = "order")
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
     private List<OrderItem> orderItems = new ArrayList<>();
 
 

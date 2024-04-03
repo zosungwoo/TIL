6e90890fea4a31868c4d3ded2391ef4be3751298
Sungwoo Cho <seouk77@gmail.com>
2024-04-03 21:28:33 +0900

    [JPA] 값 타입 / 임베디드 타입 매핑

---
+++ Spring/JPA/ex2-jpashop/src/main/java/jpashop/domain/Address.java
@@ -0,0 +1,52 @@
+package jpashop.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+
+import java.util.Objects;
+
+@Embeddable
+public class Address {
+
+    @Column(length = 10)
+    private String city;
+    @Column(length = 20)
+    private String street;
+    @Column(length = 5)
+    private String zipcode;
+
+    // 이런 의미있는 메서드를 만들어줄 수도 있음
+    public String fullAddress() {
+        return getCity() + " " + getStreet() + " " + getZipcode();
+    }
+
+    public boolean isValid(){
+        // ~~~
+        return true;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public String getZipcode() {
+        return zipcode;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Address address = (Address) o;
+        return Objects.equals(getCity(), address.getCity()) && Objects.equals(getStreet(), address.getStreet()) && Objects.equals(getZipcode(), address.getZipcode());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getCity(), getStreet(), getZipcode());
+    }
+}
--- Spring/JPA/ex2-jpashop/src/main/java/jpashop/domain/Delivery.java
+++ Spring/JPA/ex2-jpashop/src/main/java/jpashop/domain/Delivery.java
@@ -7,11 +7,8 @@ public class Delivery extends BaseEntity {
     @Id @GeneratedValue
     private Long id;
 
-    private String city;
-
-    private String street;
-
-    private String zipcode;
+    @Embedded
+    private Address address;
 
     private DeliveryStatus status;
 

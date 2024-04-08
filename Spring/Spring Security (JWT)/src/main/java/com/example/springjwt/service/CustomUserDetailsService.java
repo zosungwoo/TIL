f764e72fa8faa8ee7fd04451da2f2088cf32259b
Sungwoo Cho <seouk77@gmail.com>
2024-04-08 15:57:01 +0900

    [Spring Security + JWT] 로그인 구현 - DB 기반 로그인 검증 로직

---
+++ Spring/Spring Security (JWT)/src/main/java/com/example/springjwt/service/CustomUserDetailsService.java
@@ -0,0 +1,32 @@
+package com.example.springjwt.service;
+
+import com.example.springjwt.dto.CustomUserDetails;
+import com.example.springjwt.entity.UserEntity;
+import com.example.springjwt.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        // DB에서 조회
+        UserEntity userData = userRepository.findByUsername(username);
+
+        if (userData != null) {
+            // UserDetails에 담아서 return하면 AutneticationManager가 검증함
+            return new CustomUserDetails(userData);
+        }
+
+        return null;
+    }
+}

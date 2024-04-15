029a19a5ea65ed044d95d833969bd43b7d8e1a2b
Sungwoo Cho <seouk77@gmail.com>
2024-04-15 14:49:34 +0900

    [Spring Security + JWT] Refresh Token Rotation

--- Spring/Spring Security (JWT)/src/main/java/com/example/springjwt/controller/ReissueController.java
+++ Spring/Spring Security (JWT)/src/main/java/com/example/springjwt/controller/ReissueController.java
@@ -60,10 +60,24 @@ public class ReissueController {
         String role = jwtUtil.getRole(refresh);
 
         // make new JWT
-        String newAccess = jwtUtil.createJwt("access", username, role, 600000L);
+        String newAccess = jwtUtil.createJwt("access", username, role, 600000L);  // 10분
+        String newRefresh = jwtUtil.createJwt("refresh", username, role, 86400000L);  // 24시간
 
         // response
         response.setHeader("access", newAccess);
+        response.addCookie(createCookie("refresh", newRefresh));
+
         return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    private Cookie createCookie(String key, String value) {
+
+        Cookie cookie = new Cookie(key, value);
+        cookie.setMaxAge(24*60*60);
+        //cookie.setSecure(true);
+        //cookie.setPath("/");
+        cookie.setHttpOnly(true);
+
+        return cookie;
     }
 }

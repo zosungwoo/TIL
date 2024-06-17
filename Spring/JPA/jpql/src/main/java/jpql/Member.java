b130126107ad183475918de4251678b7c1314671
Sungwoo Cho <seouk77@gmail.com>
2024-06-17 16:43:36 +0900

    [JPA] JPQL 프로젝트 생성

---
+++ Spring/JPA/jpql/src/main/java/jpql/Member.java
@@ -0,0 +1,41 @@
+package jpql;
+
+import jakarta.persistence.*;
+
+
+@Entity
+public class Member {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private int age;
+
+    @ManyToOne
+    @JoinColumn(name = "TEAM_ID")
+    private Team team;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}

46461f8291a0e34957315cab05371c4234bdce6a
Sungwoo Cho <seouk77@gmail.com>
2024-06-19 17:15:00 +0900

    [JPA] JPQL - 프로젝션

--- Spring/JPA/jpql/src/main/java/jpql/JpaMain.java
+++ Spring/JPA/jpql/src/main/java/jpql/JpaMain.java
@@ -21,15 +21,24 @@ public class JpaMain {
             member.setAge(10);
             em.persist(member);
 
-            TypedQuery<Member> query = em.createQuery("select m from Member m where m.username = :username", Member.class);
-            query.setParameter("username", "member1");
-            List<Member> resultList = query.getResultList();
-//            Member singleResult = query.getSingleResult();  -> 반드시 하나여야함 (0 or <= 2 라면 예외 발생)
-//            System.out.println(singleResult);
+//            TypedQuery<Member> query = em.createQuery("select m from Member m where m.username = :username", Member.class);
+//            query.setParameter("username", "member1");
+//            List<Member> resultList = query.getResultList();
+////            Member singleResult = query.getSingleResult();  -> 반드시 하나여야함 (0 or <= 2 라면 예외 발생)
+////            System.out.println(singleResult);
+//
+//            for (Member member1 : resultList) {
+//                System.out.println(member1);
+//            }
 
-            for (Member member1 : resultList) {
-                System.out.println(member1);
-            }
+//            List<Member> resultList = em.createQuery("select m from Member m", Member.class)
+//                    .getResultList();
+
+            List<MemberDto> resultList = em.createQuery("select new jpql.MemberDto(m.username, m.age) from Member m", MemberDto.class)
+                    .getResultList();
+            System.out.println(resultList.get(0).getUsername());
+            System.out.println(resultList.get(0).getAge());
+
 
             tx.commit();
         } catch (Exception e) {
